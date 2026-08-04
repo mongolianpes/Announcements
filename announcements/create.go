39c556f397f5454e1f8133a5f9bddc924e59aeef
customer <cust@gmail.com>
2026-08-04 17:35:56 +0300

    Изменен способ общения с клиентом для создания объявления

--- announcements/create.go
+++ announcements/create.go
@@ -1,7 +1,7 @@
 package announcements
 
 import (
-	"io"
+	"errors"
 
 	"github.com/lib/pq"
 
@@ -10,44 +10,55 @@ import (
 )
 
 func (s *AnnouncementsServer) CreateAnnouncement(stream pb.Announcements_CreateAnnouncementServer) error {
+	req, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+
 	data := &pb.CreateAnnouncementResponse{}
 
-	var announcementID int
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			return stream.Send(&pb.CreateAnnouncementResponse{})
+	const queryAddAnnouncementInfo = "INSERT INTO announcements (title, description, category, announcement_author_id) VALUES ($1, $2, $3, $4) RETURNING announcement_id"
+	if err := db.QueryRow(queryAddAnnouncementInfo, req.Title, req.Description, req.Category, req.AuthorID).Scan(&data.AnnouncementID); err != nil {
+		var errCause string
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code {
+			case "22001":
+				errCause = "Название или описание слишком длинное. Для нзвания используйте до 10 символов, для описания до 100"
+			default:
+				errCause = "Неизвестная ошибка, попробуйте позже"
+			}
+		} else {
+			errCause = "Неизвестная ошибка, попробуйте позже"
 		}
-		if err != nil {
+		return errors.New(errCause)
+	}
+
+	if err := stream.SendAndClose(data); err != nil {
+		return err
+	}
+
+	go embedding.InsertEmbedding(db, int(data.AnnouncementID), req.Title+req.Description, "UPDATE announcements SET embedding = $1::float8[] WHERE announcement_id = $2")
+
+	return nil
+}
+
+func (s *AnnouncementsServer) AddImages(stream pb.Announcements_AddImagesServer) error {
+	req, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+
+	data := &pb.AddImagesResponse{}
+
+	if len(req.ImagesPath) > 0 {
+		if _, err := db.Exec("UPDATE announcements SET images_path = $1 WHERE announcement_id = $2", req.ImagesPath, req.AnnouncementID); err != nil {
 			return err
 		}
-
-		if len(req.ImagesPath) >= 1 {
-			if _, err := db.Exec("UPDATE announcements SET images_path = $1 WHERE announcement_id = $2", req.ImagesPath, announcementID); err != nil {
-				data.Error = "Картинки не сохранены"
-			} else {
-				data.Error = ""
-			}
-			return stream.Send(data)
-		} else {
-			if err := db.QueryRow("INSERT INTO announcements (title, description, category, announcement_author_id, images_path) VALUES ($1, $2, $3, $4, $5) RETURNING announcement_id", req.Title, req.Description, req.Category, req.AuthorID, req.ImagesPath).Scan(&announcementID); err != nil {
-				if pqErr, ok := err.(*pq.Error); ok {
-					switch pqErr.Code {
-					case "22001":
-						data.Error = "Название или описание слишком длинное. Для нзвания используйте до 10 символов, для описания до 100"
-					default:
-						data.Error = "Неизвестная ошибка, попробуйте позже"
-					}
-				} else {
-					data.Error = "Неизвестная ошибка, попробуйте позже"
-				}
-			} else {
-				data.Error = ""
-			}
-
-			go embedding.InsertEmbedding(db, announcementID, req.Title+req.Description, "UPDATE announcements SET embedding = $1::float8[] WHERE announcement_id = $1")
-
-			stream.Send(data)
-		}
 	}
+
+	if err := stream.SendAndClose(data); err != nil {
+		return err
+	}
+
+	return nil
 }
